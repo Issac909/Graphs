6dc83738330503a3201d7306926b1a2b42abcd7a
Issac Moreno <issaxavier909@gmail.com>
2020-08-13 18:05:33 -0700

    Thursday

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -2,6 +2,8 @@ from room import Room
 from player import Player
 from world import World
 
+import os
+
 import random
 from ast import literal_eval
 
@@ -10,11 +12,12 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+dirpath = os.path.dirname(os.path.abspath(__file__))
+map_file = dirpath + "/maps/main_maze.txt"
+# map_file = dirpath + "maps/test_cross.txt"
+# map_file = dirpath + "maps/test_loop.txt"
+# map_file = dirpath + "maps/test_loop_fork.txt"
+# map_file = dirpath + "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -26,9 +29,30 @@ world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
 traversal_path = []
 
+visited =  {}
+reverse_path = [] 
+o = {"n" : "s", "e": "w", "s":"n", "w": "e" } 
+
+visited[player.current_room.id] = player.current_room.get_exits()
+
+while len(visited) > len(room_graph):
+    if player.current_room.id not in visited: 
+        visited[player.current_room.id] = player.current_room.get_exits()
+        prev = reverse_path[-1]
+        visited[player.currrent_room.id].remove(prev)
+    if len(visited[player.current_room.id]) == 0: 
+        prev = reverse_path[-1]
+        reverse_path.pop()
+        traversal_path.append(prev)
+        player.travel(prev)
+    else:
+        d = visited[player.current_room.id][-1]
+        visited[player.current_room.id].pop()
+        traversal_path.append(d) 
+        reverse_path.append(o[d])
+        player.travel(d)
 
 
 # TRAVERSAL TEST
