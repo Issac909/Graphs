fc5d9d5acbe096b195373b37c4355037ef5764b3
Issac Moreno <issaxavier909@gmail.com>
2020-08-14 10:03:48 -0700

    Friday - Sprint MVP = 1004 moves, 500 rooms

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -13,11 +13,11 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 dirpath = os.path.dirname(os.path.abspath(__file__))
-map_file = dirpath + "/maps/main_maze.txt"
+# map_file = dirpath + "/maps/main_maze.txt"
 # map_file = dirpath + "maps/test_cross.txt"
 # map_file = dirpath + "maps/test_loop.txt"
 # map_file = dirpath + "maps/test_loop_fork.txt"
-# map_file = dirpath + "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -30,30 +30,44 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 traversal_path = []
-
+# Rooms visited
 visited =  {}
-reverse_path = [] 
+# Used to store the opposite direction for back travel
 direction = {"n" : "s", "e": "w", "s":"n", "w": "e" } 
-
-visited[player.current_room.id] = player.current_room.get_exits()
-
-while len(visited) > len(room_graph):
+# Store the directions
+reverse_path = [] 
+# marking the id of room as exit
+visited[0] = player.current_room.get_exits()
+# while rooms visited is less than the total number of rooms
+while len(visited) < len(room_graph) - 1:
+    # if room is not visited yet
     if player.current_room.id not in visited: 
+        # mark the exits from the current room
         visited[player.current_room.id] = player.current_room.get_exits()
+        # store the previous room traveled
         prev = reverse_path[-1]
-        visited[player.currrent_room.id].remove(prev)
-    if len(visited[player.current_room.id]) == 0: 
-        prev = reverse_path[-1]
-        reverse_path.pop()
-        traversal_path.append(prev)
-        player.travel(prev)
-    else:
-        d = visited[player.current_room.id][-1]
-        visited[player.current_room.id].pop()
-        traversal_path.append(d) 
-        reverse_path.append(direction[d])
-        player.travel(d)
+        # remove last exit from exits dictionary
+        visited[player.current_room.id].remove(prev)
+        
+    # when no rooms to explore
+    while len(visited[player.current_room.id]) < 1: 
+        # store the last direction
+        reverse = reverse_path.pop()
+        # add the reverse direction to traversal path
+        traversal_path.append(reverse)
+        # move player to previous location
+        player.travel(reverse)
+    
+    # store the first exit in []
+    exit_way = visited[player.current_room.id].pop(0)
+    # add exit to traversal path
+    traversal_path.append(exit_way) 
+    # add to reverse path using direction entered from 
+    reverse_path.append(direction[exit_way])
+    # move player to first exit in []
+    player.travel(exit_way)
 
+# 1004 moves, 500 rooms
 
 # TRAVERSAL TEST
 visited_rooms = set()
